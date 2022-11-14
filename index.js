90c0228f6be0e57e055f0e798f44bfec9a3ebd8c
gamesreboot <69279322+gamesreboot@users.noreply.github.com>
2022-11-13 21:45:59 -0700

    format

--- index.js
+++ index.js
@@ -45,30 +45,32 @@ function between(min, max) {
 const token = process.env.TOKEN
 
 client.on("messageCreate", msg => {
+	let data = db.data;
 	if (msg.author.bot) {
 		return
 	}
-	if(!db.data[msg.author.id]){
-		db.data[msg.author.id] = {
+	if(!data[msg.author.id]){
+		data[msg.author.id] = {
 			"xp": 0,
 			"Levels": 1,
 			"lastmsg": 0
 		}
 		db.write()
 	}
-	if (db.data[msg.author.id].lastmsg === 0 ){
-		db.data[msg.author.id].lastmsg = Date.now();
+	let UD = data[msg.author.id];
+	if (UD.lastmsg === 0 ){
+		UD.lastmsg = Date.now();
 		db.write()
 	}
-	if (db.data[msg.author.id]) {
-		if (Date.now() - db.data[msg.author.id].lastmsg >= 65000) {
-			db.data[msg.author.id].xp += between(10,12)
-			db.data[msg.author.id].lastmsg = Date.now();
-			if (db.data[msg.author.id].xp > (db.data[msg.author.id].Levels * 112) && db.data[msg.author.id].xp < ((db.data[msg.author.id].Levels + 1) * 112)) {
-				db.data[msg.author.id].Levels += 1
-				msg.channel.send("<@" + msg.author.id + ">" + ", you advanced to level " + db.data[msg.author.id].Levels + ".")
+	if (UD) {
+		if (Date.now() - UD.lastmsg >= 65000) {
+			UD.xp += between(10,12)
+			UD.lastmsg = Date.now();
+			if (UD.xp > (UD.Levels * 112) && UD.xp < ((UD.Levels + 1) * 112)) {
+				UD.Levels += 1
+				msg.channel.send("<@" + msg.author.id + ">" + ", you advanced to level " + UD.Levels + ".")
 				for (const v in db2.data) {
-					if (db.data[msg.author.id].Levels >= db2.data[v].level) {
+					if (UD.Levels >= db2.data[v].level) {
 						let role = msg.guild.roles.cache.find(role => role.id === db2.data[v].ID.toString());
 						if (msg.member.roles.cache.has(role.id)) {} else {
 							msg.member.roles.add(role)
@@ -86,7 +88,6 @@ client.on("messageCreate", msg => {
 		}
 		db.write()
 	}
-
 });
 
 client.on("messageCreate", msg => {
@@ -95,7 +96,7 @@ client.on("messageCreate", msg => {
 	const command = args.shift().toLowerCase();
 	var arg = ""
 	if (command === "rank") {
-	  msg.channel.send(":warning: legacy commands will soon be deprecated only use this for seeing other users ranks :warning:")
+	  	msg.channel.send(":warning: legacy commands will soon be deprecated only use this for seeing other users ranks :warning:")
 		if (!args.length) {
 			arg = msg.author.id
 		} else {
@@ -106,13 +107,14 @@ client.on("messageCreate", msg => {
 				arg = vcr
 			}
 		}
+		let data = db.data[arg]
 		let user = client.users.cache.find(user => user.id === arg)
 		if (user != undefined) {
 			msg.channel.send({
 				embeds: [{
 					"type": "rich",
 					"title": `Users Rank`,
-					"description": "current stats\nid: " + arg + " \nXP: " + db.data[arg].xp + "\nLEVEL: " + db.data[arg].Levels + "\nUsername: " + user.username,
+					"description": "current stats\nid: " + arg + " \nXP: " + data.xp + "\nLEVEL: " + data.Levels + "\nUsername: " + user.username,
 					"color": 0x0073ff,
 				}]
 			})
@@ -121,7 +123,7 @@ client.on("messageCreate", msg => {
 				embeds: [{
 					"type": "rich",
 					"title": `Users Rank`,
-					"description": "current stats\nid: " + arg + " \nXP: " + db.data[arg].xp + "\nLEVEL: " + db.data[arg].Levels + "\nUsername: " + "username not found",
+					"description": "current stats\nid: " + arg + " \nXP: " + data.xp + "\nLEVEL: " + data.Levels + "\nUsername: " + "username not found",
 					"color": 0x0073ff,
 				}]
 			})
@@ -166,17 +168,18 @@ client.on("messageCreate", msg => {
 
 	if (command === "purge" || command == "clear") {
 		if (!msg.member.permissions.has("ADMINISTRATOR")) {
-					msg.channel.send({
+			msg.channel.send({
 				embeds: [{
 					"type": "rich",
 					"title": `Purged`,
 					"description": "You need to be an administrator to use this command.",
 					"color": 0x0073ff
-				}]});
+				}]
+			});
 			return
 		} 
 		if (args[0] && !isNaN(+args[0])) {
-		  msg.delete()
+		  	msg.delete()
 			msg.channel.bulkDelete(parseInt(args[0]))
 			var a = msg.channel.send({
 				embeds: [{
@@ -186,7 +189,7 @@ client.on("messageCreate", msg => {
 					"color": 0x0073ff
 				}]
 			}).then(a => setTimeout(function() {
-				a.delete()
+				a.delete();
 			}, 3000));
 		
 		} else {
@@ -209,11 +212,11 @@ client.on("messageCreate", msg => {
 
 	if (command === "verify") {
 		if (msg.channel.id === "923453266916966420") {
-			let role = msg.guild.roles.cache.find(role => role.id === "923443703987724289")
-			msg.member.roles.add(role)
+			let role = msg.guild.roles.cache.find(role => role.id === "923443703987724289");
+			msg.member.roles.add(role);
 			if (msg.member.roles.cache.has("928470213043830824")) {
-				let role = msg.guild.roles.cache.find(role => role.id === "928470213043830824")
-				msg.member.roles.remove(role)
+				let role = msg.guild.roles.cache.find(role => role.id === "928470213043830824");
+				msg.member.roles.remove(role);
 			}
 			msg.channel.send({
 				embeds: [{
@@ -231,23 +234,24 @@ client.on(Events.InteractionCreate, interaction => {
 	if (interaction.commandName == "ping"){
 		interaction.reply("Pong!");
   	} else if(interaction.commandName == "rank"){
-		let arg = interaction.user.id
-		let user = client.users.cache.find(user => user.id === arg)
+		let arg = interaction.user.id;
+		let user = client.users.cache.find(user => user.id === arg);
+		let data = db.data[arg];
 		if (user != undefined) {
 			interaction.reply({
-			embeds: [{
-				"type": "rich",
-				"title": `Users Rank`,
-				"description": "current stats\nid: " + arg + " \nXP: " + db.data[arg].xp + "\nLEVEL: " + db.data[arg].Levels + "\nUsername: " + user.username,
-				"color": 0x0073ff,
-			}]
-	 		})
+				embeds: [{
+					"type": "rich",
+					"title": `Users Rank`,
+					"description": "current stats\nid: " + arg + " \nXP: " + data.xp + "\nLEVEL: " + data.Levels + "\nUsername: " + user.username,
+					"color": 0x0073ff,
+				}]
+	 		});
 	 	 } else {
 			interaction.reply({
 			  	embeds: [{
 					"type": "rich",
 					"title": `Users Rank`,
-					"description": "current stats\nid: " + arg + " \nXP: " + db.data[arg].xp + "\nLEVEL: " + db.data[arg].Levels + "\nUsername: " + "username not found",
+					"description": "current stats\nid: " + arg + " \nXP: " + data.xp + "\nLEVEL: " + data.Levels + "\nUsername: " + "username not found",
 					"color": 0x0073ff,
 				}]
 			});
@@ -263,18 +267,18 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 
 (async ()=>{
   	await client.login(token).catch((err) => {
-		throw err
+		throw err;
   	});
 
   	const rest = new REST({ version: "10" }).setToken(token);
 	await rest.put(Routes.applicationCommands(client.user.id), { body: [
-	{
-		name: "ping",
-		description: 'returns "Pong!" back to the user'
-	},
-	{
-		name: "rank",
-		description: "returns the users ranks",
-	}
-  ]});
+		{
+			name: "ping",
+			description: 'returns "Pong!" back to the user'
+		},
+		{
+			name: "rank",
+			description: "returns the users ranks"
+		}
+  	]});
 })();
